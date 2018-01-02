59c5a6f6d31fc35acda59a3cf6012456d41d6a80
ZhangXiaoD <156678702@qq.com>
2018-01-02 01:45:48 -0800

    123

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,25 +1,16 @@
 from .models import Goods
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 from .serializers import GoodsSerializer
 # Create your views here.
 
 
-class GoodsListView(APIView):
+class GoodsListView(generics.ListAPIView):
     """
     商品列表页
     """
-
-    def get(self, request):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
-
-    def post(self, request):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
